fb89451684ab11f8a9b1a26b04ccbdbe73185058
Abhinav Karpur <96277364+at5707@users.noreply.github.com>
2025-06-03 10:51:43 +0530

    Create JavaStringTokens.java

---
+++ JavaStringTokens.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        scan.close();
+        s=s.trim();
+        if (s.length()==0) {
+            System.out.println(0);
+            return;
+        }
+        String[] tokens=s.split("[^A-Za-z]+");
+        System.out.println(tokens.length);
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+    }
+}
